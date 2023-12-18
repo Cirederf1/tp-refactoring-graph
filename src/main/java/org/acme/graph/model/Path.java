cee594ecda92d8bc5fadb4120d8dd295881c4679
Cirederf1 <fredericye@hotmail.fr>
2023-12-18 16:06:15 +0100

    0.6 - Amélioration du rendu des chemins

---
+++ src/main/java/org/acme/graph/model/Path.java
@@ -0,0 +1,28 @@
+package org.acme.graph.model;
+
+import java.util.List;
+
+public class Path {
+	private List<Edge> edges;
+	
+	public Path(List<Edge> edges) {
+		this.edges = edges;
+	}
+	
+	public int getLength() {
+		int result = 0;
+		for (Edge edge: edges) {
+			result += edge.getGeometry().getLength();
+		}
+		
+		return result;
+	}
+	
+	public int size() {
+		return edges.size();
+	}
+	
+	public Edge get(int index) {
+		return edges.get(index);
+	}
+}
